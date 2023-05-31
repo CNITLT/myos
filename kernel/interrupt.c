201086cd0492dc5848a6c148c9acc6bdd1a66a28
cnitlt <cnitlt@foxmail.com>
2023-05-31 20:46:33 +0800

    8259A设置完，测试了下中断，int指令触发的中断不会产生错误码，所以main函数从int 8开始一直递归中断，因为把返回地址弄没了

--- kernel/interrupt.c
+++ kernel/interrupt.c
@@ -1,10 +1,40 @@
 #include "interrupt.h"
 #include "print.h"
+#include "io.h"
+
+//中断门描述符定义
+typedef struct interrupt_gate_desc{
+    uint16_t func_offset_low; //中断处理程序在目标代码段的偏移中的低16位
+    uint16_t code_selector; //目标代码段选择子
+    struct {
+        uint16_t fixed:11; //对中断门来说是固定的几位，高地址110 0000 0000低地址 
+        uint16_t D:1; //D为1表示32位，为0是16位
+        uint16_t S:1; //固定的为0
+        uint16_t DPL:2; //特权级
+        uint16_t P:1; //存在位
+    } attribute;
+    uint16_t func_offset_high; //中断处理程序在目标代码段的偏移中的高16位
+} interrupt_gate_desc;
+
+#define DEFAULT_INTERRUPT_GATE_DESC_VALUE {0,(1 << 3),{0x600,1,0,0,1},0}
+#define IDT_SIZE 32
+
+
 #define ERROR_CODE_RET asm volatile("addl $4,%esp;iret;");
 #define DEFAULT_RET asm volatile("iret;");
+#define PIC_EOI asm volatile(" \
+    push %eax; \
+    movb $0x20, %al; \
+    out %al,$0xA0; \
+    out %al,$0x20; \
+    pop %eax; \
+");
+#define DEFAULT_EOI asm volatile("nop;");
+
+
 #define NAKEDFUNC __attribute__((naked))
 //代理中断函数，主要用来处理返回到正确的地方,普通函数都是ret返回
-#define IDT_FUNC_ENTRY_PROXY(INTERRUPT_NUM, RET_TYPE) \
+#define IDT_FUNC_ENTRY_PROXY(INTERRUPT_NUM, RET_TYPE, EOI_TYPE) \
 NAKEDFUNC static void  IDT_FUNC_ENTRY_PROXY##INTERRUPT_NUM(void){ \
     asm volatile (" \
     push %ds; \
@@ -15,6 +45,7 @@ NAKEDFUNC static void  IDT_FUNC_ENTRY_PROXY##INTERRUPT_NUM(void){ \
     asm volatile(" \
     call *%0; \
     "::"m"(IDT_FUNCS[INTERRUPT_NUM])); \
+    EOI_TYPE \
     asm volatile(" \
     popal; \
     pop %gs; \
@@ -25,40 +56,41 @@ NAKEDFUNC static void  IDT_FUNC_ENTRY_PROXY##INTERRUPT_NUM(void){ \
 } 
 
 static interrupt_func_handler IDT_FUNCS[IDT_SIZE];//实际的处理函数
-interrupt_gate_desc IDT[IDT_SIZE];
 
-IDT_FUNC_ENTRY_PROXY(0,DEFAULT_RET)
-IDT_FUNC_ENTRY_PROXY(1,DEFAULT_RET)
-IDT_FUNC_ENTRY_PROXY(2,DEFAULT_RET)
-IDT_FUNC_ENTRY_PROXY(3,DEFAULT_RET)
-IDT_FUNC_ENTRY_PROXY(4,DEFAULT_RET)
-IDT_FUNC_ENTRY_PROXY(5,DEFAULT_RET)
-IDT_FUNC_ENTRY_PROXY(6,DEFAULT_RET)
-IDT_FUNC_ENTRY_PROXY(7,DEFAULT_RET)
-IDT_FUNC_ENTRY_PROXY(8,ERROR_CODE_RET)
-IDT_FUNC_ENTRY_PROXY(9,DEFAULT_RET)
-IDT_FUNC_ENTRY_PROXY(10,ERROR_CODE_RET)
-IDT_FUNC_ENTRY_PROXY(11,ERROR_CODE_RET)
-IDT_FUNC_ENTRY_PROXY(12,ERROR_CODE_RET)
-IDT_FUNC_ENTRY_PROXY(13,ERROR_CODE_RET)
-IDT_FUNC_ENTRY_PROXY(14,ERROR_CODE_RET)
-IDT_FUNC_ENTRY_PROXY(15,DEFAULT_RET)
-IDT_FUNC_ENTRY_PROXY(16,DEFAULT_RET)
-IDT_FUNC_ENTRY_PROXY(17,ERROR_CODE_RET)
-IDT_FUNC_ENTRY_PROXY(18,DEFAULT_RET)
-IDT_FUNC_ENTRY_PROXY(19,DEFAULT_RET)
-IDT_FUNC_ENTRY_PROXY(20,DEFAULT_RET)
-IDT_FUNC_ENTRY_PROXY(21,DEFAULT_RET)
-IDT_FUNC_ENTRY_PROXY(22,DEFAULT_RET)
-IDT_FUNC_ENTRY_PROXY(23,DEFAULT_RET)
-IDT_FUNC_ENTRY_PROXY(24,DEFAULT_RET)
-IDT_FUNC_ENTRY_PROXY(25,DEFAULT_RET)
-IDT_FUNC_ENTRY_PROXY(26,DEFAULT_RET)
-IDT_FUNC_ENTRY_PROXY(27,DEFAULT_RET)
-IDT_FUNC_ENTRY_PROXY(28,DEFAULT_RET)
-IDT_FUNC_ENTRY_PROXY(29,DEFAULT_RET)
-IDT_FUNC_ENTRY_PROXY(30,DEFAULT_RET)
-IDT_FUNC_ENTRY_PROXY(31,DEFAULT_RET)
+
+interrupt_gate_desc IDT[IDT_SIZE];
+IDT_FUNC_ENTRY_PROXY(0,DEFAULT_RET,DEFAULT_EOI)
+IDT_FUNC_ENTRY_PROXY(1,DEFAULT_RET,DEFAULT_EOI)
+IDT_FUNC_ENTRY_PROXY(2,DEFAULT_RET,DEFAULT_EOI)
+IDT_FUNC_ENTRY_PROXY(3,DEFAULT_RET,DEFAULT_EOI)
+IDT_FUNC_ENTRY_PROXY(4,DEFAULT_RET,DEFAULT_EOI)
+IDT_FUNC_ENTRY_PROXY(5,DEFAULT_RET,DEFAULT_EOI)
+IDT_FUNC_ENTRY_PROXY(6,DEFAULT_RET,DEFAULT_EOI)
+IDT_FUNC_ENTRY_PROXY(7,DEFAULT_RET,DEFAULT_EOI)
+IDT_FUNC_ENTRY_PROXY(8,ERROR_CODE_RET,DEFAULT_EOI)
+IDT_FUNC_ENTRY_PROXY(9,DEFAULT_RET,DEFAULT_EOI)
+IDT_FUNC_ENTRY_PROXY(10,ERROR_CODE_RET,DEFAULT_EOI)
+IDT_FUNC_ENTRY_PROXY(11,ERROR_CODE_RET,DEFAULT_EOI)
+IDT_FUNC_ENTRY_PROXY(12,ERROR_CODE_RET,DEFAULT_EOI)
+IDT_FUNC_ENTRY_PROXY(13,ERROR_CODE_RET,DEFAULT_EOI)
+IDT_FUNC_ENTRY_PROXY(14,ERROR_CODE_RET,DEFAULT_EOI)
+IDT_FUNC_ENTRY_PROXY(15,DEFAULT_RET,DEFAULT_EOI)
+IDT_FUNC_ENTRY_PROXY(16,DEFAULT_RET,DEFAULT_EOI)
+IDT_FUNC_ENTRY_PROXY(17,ERROR_CODE_RET,DEFAULT_EOI)
+IDT_FUNC_ENTRY_PROXY(18,DEFAULT_RET,DEFAULT_EOI)
+IDT_FUNC_ENTRY_PROXY(19,DEFAULT_RET,DEFAULT_EOI)
+IDT_FUNC_ENTRY_PROXY(20,DEFAULT_RET,PIC_EOI)
+IDT_FUNC_ENTRY_PROXY(21,DEFAULT_RET,PIC_EOI)
+IDT_FUNC_ENTRY_PROXY(22,DEFAULT_RET,PIC_EOI)
+IDT_FUNC_ENTRY_PROXY(23,DEFAULT_RET,PIC_EOI)
+IDT_FUNC_ENTRY_PROXY(24,DEFAULT_RET,PIC_EOI)
+IDT_FUNC_ENTRY_PROXY(25,DEFAULT_RET,PIC_EOI)
+IDT_FUNC_ENTRY_PROXY(26,DEFAULT_RET,PIC_EOI)
+IDT_FUNC_ENTRY_PROXY(27,DEFAULT_RET,PIC_EOI)
+IDT_FUNC_ENTRY_PROXY(28,DEFAULT_RET,PIC_EOI)
+IDT_FUNC_ENTRY_PROXY(29,DEFAULT_RET,PIC_EOI)
+IDT_FUNC_ENTRY_PROXY(30,DEFAULT_RET,PIC_EOI)
+IDT_FUNC_ENTRY_PROXY(31,DEFAULT_RET,PIC_EOI)
 
 interrupt_func_handler IDT_ENTRY_PROXYS[IDT_SIZE] = {
    IDT_FUNC_ENTRY_PROXY0,
@@ -95,13 +127,81 @@ interrupt_func_handler IDT_ENTRY_PROXYS[IDT_SIZE] = {
    IDT_FUNC_ENTRY_PROXY31
 }; //代理函数的地址
 
+
+/* 
+   前20个中断的意义
+   intr_name[0] = "#DE Divide Error";
+   intr_name[1] = "#DB Debug Exception";
+   intr_name[2] = "NMI Interrupt";
+   intr_name[3] = "#BP Breakpoint Exception";
+   intr_name[4] = "#OF Overflow Exception";
+   intr_name[5] = "#BR BOUND Range Exceeded Exception";
+   intr_name[6] = "#UD Invalid Opcode Exception";
+   intr_name[7] = "#NM Device Not Available Exception";
+   intr_name[8] = "#DF Double Fault Exception";
+   intr_name[9] = "Coprocessor Segment Overrun";
+   intr_name[10] = "#TS Invalid TSS Exception";
+   intr_name[11] = "#NP Segment Not Present";
+   intr_name[12] = "#SS Stack Fault Exception";
+   intr_name[13] = "#GP General Protection Exception";
+   intr_name[14] = "#PF Page-Fault Exception";
+   // intr_name[15] 第15项是intel保留项，未使用
+   intr_name[16] = "#MF x87 FPU Floating-Point Error";
+   intr_name[17] = "#AC Alignment Check Exception";
+   intr_name[18] = "#MC Machine-Check Exception";
+   intr_name[19] = "#XF SIMD Floating-Point Exception";
+*/
 static const interrupt_gate_desc default_value = DEFAULT_INTERRUPT_GATE_DESC_VALUE;
-void idt_desc_init(void){
+
+/*
+@brief 中断向量表的初始化
+*/
+static void idt_desc_init(void){
     for(int i = 0; i < IDT_SIZE; i++){
         IDT[i] = default_value;
         IDT[i].func_offset_low = (uint32_t)IDT_ENTRY_PROXYS[i] & 0x0000FFFF;
         IDT[i].func_offset_high = ((uint32_t)IDT_ENTRY_PROXYS[i] & 0xFFFF0000) >> 16; 
     }
+}
+
+#define PIC_8259A_MASTER_ICW1_PORT 0x20
+#define PIC_8259A_MASTER_ICW2_PORT 0x21
+#define PIC_8259A_MASTER_ICW3_PORT 0x21
+#define PIC_8259A_MASTER_ICW4_PORT 0x21
+
+#define PIC_8259A_SLAVE_ICW1_PORT 0xA0
+#define PIC_8259A_SLAVE_ICW2_PORT 0xA1
+#define PIC_8259A_SLAVE_ICW3_PORT 0xA1
+#define PIC_8259A_SLAVE_ICW4_PORT 0xA1
+
+#define PIC_8259A_MASTER_OCW1_PORT 0x21
+#define PIC_8259A_MASTER_OCW2_PORT 0x20
+#define PIC_8259A_MASTER_OCW3_PORT 0x20
+
+#define PIC_8259A_SLAVE_OCW1_PORT 0xA1
+#define PIC_8259A_SLAVE_OCW2_PORT 0xA0
+#define PIC_8259A_SLAVE_OCW3_PORT 0xA0
+
+/*
+@brief 可编程中断控制器的初始化，这里是8259A
+*/
+static void pic_init(void){
+    
+   /* 初始化主片 */
+   outb (PIC_8259A_MASTER_ICW1_PORT, 0x11);   // ICW1: 边沿触发,级联8259, 需要ICW4.
+   outb (PIC_8259A_MASTER_ICW2_PORT, 0x20);   // ICW2: 起始中断向量号为0x20,也就是IR[0-7] 为 0x20 ~ 0x27.
+   outb (PIC_8259A_MASTER_ICW3_PORT, 0x04);   // ICW3: IR2接从片. 
+   outb (PIC_8259A_MASTER_ICW4_PORT, 0x01);   // ICW4: 8086模式, 正常EOI
+
+   /* 初始化从片 */
+   outb (PIC_8259A_SLAVE_ICW1_PORT, 0x11);    // ICW1: 边沿触发,级联8259, 需要ICW4.
+   outb (PIC_8259A_SLAVE_ICW2_PORT, 0x28);    // ICW2: 起始中断向量号为0x28,也就是IR[8-15] 为 0x28 ~ 0x2F.
+   outb (PIC_8259A_SLAVE_ICW3_PORT, 0x02);    // ICW3: 设置从片连接到主片的IR2引脚
+   outb (PIC_8259A_SLAVE_ICW4_PORT, 0x01);    // ICW4: 8086模式, 正常EOI
+
+   /* 打开主片上IR0,也就是目前只接受时钟产生的中断 */
+   outb (PIC_8259A_MASTER_OCW1_PORT, 0xfe);
+   outb (PIC_8259A_SLAVE_OCW1_PORT, 0xff);
 }
 
 interrupt_func_handler register_interrupt_func(uint16_t INTERRUPT_NUM, interrupt_func_handler func){
@@ -126,7 +226,7 @@ static void register_default_func(void){
 void interrupt_init(){
     idt_desc_init();
     register_default_func();
-
+    pic_init();
     /* 加载idt */
    uint64_t idt_operand = ((sizeof(IDT) - 1) | ((uint64_t)(uint32_t)IDT << 16));
    asm volatile("lidt %0" : : "m" (idt_operand));
--- kernel/interrupt.h
+++ kernel/interrupt.h
@@ -1,30 +1,9 @@
 #ifndef __INTERRUPT_H
 #define __INTERRUPT_H
 #include "stdint.h"
+
 //实际的中断函数类型
 typedef void (*interrupt_func_handler)(void);
-//中断门描述符定义
-typedef struct interrupt_gate_desc{
-    uint16_t func_offset_low; //中断处理程序在目标代码段的偏移中的低16位
-    uint16_t code_selector; //目标代码段选择子
-    struct {
-        uint16_t fixed:11; //对中断门来说是固定的几位，高地址110 0000 0000低地址 
-        uint16_t D:1; //D为1表示32位，为0是16位
-        uint16_t S:1; //固定的为0
-        uint16_t DPL:2; //特权级
-        uint16_t P:1; //存在位
-    } attribute;
-    uint16_t func_offset_high; //中断处理程序在目标代码段的偏移中的高16位
-} interrupt_gate_desc;
-
-#define DEFAULT_INTERRUPT_GATE_DESC_VALUE {0,(1 << 3),{0x600,1,0,0,1},0}
-#define IDT_SIZE 32
-extern interrupt_gate_desc IDT[];//声明全局的IDT表，实际定义在.c文件中
-/*
-@brief 初始化idt表内所有描述符
-*/
-void idt_desc_init(void);
-
 /*
 @brief 注册中断函数
 @param INTERRUPT_NUM:uint16_t 中断向量号
