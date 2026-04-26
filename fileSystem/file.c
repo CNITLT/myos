02df4b88ac6d4f556f05f2bbe0bad37d22319021
cnitlt <cnitlt@foxmail.com>
2026-04-26 16:37:53 +0800

    安装逻辑问题修复

--- fileSystem/file.c
+++ fileSystem/file.c
@@ -19,7 +19,7 @@ int32_t get_free_file_slot_in_g_table() {
 int32_t pcb_fd_install(int32_t globa_fd_index) {
     struct task_struct *pcb = get_current_pcb();
      for(int i = USED_FD_START_INDEX; i < MAX_FD_SIZE; i++) {
-        if (pcb->fd_table[i] == 0) {
+        if (pcb->fd_table[i] == -1) {
             pcb->fd_table[i] = globa_fd_index;
             return i;
         }
